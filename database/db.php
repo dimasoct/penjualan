c2abdf22942dfa89d39fa57db52956809ee57d2a
Dimas Oktariawan <108382144+dimasoct@users.noreply.github.com>
2025-01-03 20:38:45 +0700

    db.php

--- database/db.php
+++ database/db.php
@@ -1,11 +1,12 @@
 <?php
-$host = 'localhost'; // Nama host database
-$dbname = 'koperasi'; // Nama database
-$username = 'root'; // Username database
-$password = ''; // Password database
+$host = 'x7piv.h.filess.io'; // Nama host database
+$dbname = 'appku_butterits'; // Nama database
+$username = 'appku_butterits'; // Username database
+$password = 'b43a706ce7203f4d03ddcc821b592b59d9345cd8'; // Password database
+$port = '3307'; // Port database
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Could not connect to the database $dbname :" . $e->getMessage());
